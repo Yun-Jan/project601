023d5bda5046d65cebdeb01d679eb79762e36070
yunjan <yt266@njit.edu>
2018-10-06 17:00:41 -0400

    Feature: create main class

--- public/index.php
+++ public/index.php
@@ -5,4 +5,22 @@
  * Date: 10/6/18
  * Time: 3:48 PM
  */
-echo"hello";
+main:: start();
+
+class main{
+
+    static public function start()
+    {
+        $file = fopen("example.csv", "r");
+
+        while (!feof($file)) {
+
+            $record=fgetcsv($file);
+            $records[] = $record;
+
+        }
+
+        fclose($file);
+        print_r($records);
+    }
+}
