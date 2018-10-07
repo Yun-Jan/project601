e9e7942b01c291152859e176fad33d9c9f14418f
yunjan <yt266@njit.edu>
2018-10-06 20:17:49 -0400

    Fix: Edited html class

--- public/index.php
+++ public/index.php
@@ -21,8 +21,19 @@ class html{
     public static function generateTable($records)
     {
         foreach ($records as $record) {
-            $array = $record->returnArray();
-            print_r($array);
+
+            if ($count==0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
         }
     }
 }
